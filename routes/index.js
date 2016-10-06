ec6a1ee9f57fa764e76e54b783e6d36ef0ef38f1
Ryan Gaus <rsg1egoman@gmail.com>
2016-10-06 06:49:08 -0400

    code: routes for the app

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,19 @@ router.get('/', function(req, res, next) {
 
 // Items
 router.get('/items')
-router.get('/items/:id')
+router.get('/items/:itemId')
 router.post('/items')
-router.put('/items/:id')
+router.put('/items/:itemId')
 
 // List
 router.get('/lists')
-router.get('/lists/:id')
+router.get('/lists/:listId')
 router.post('/lists')
-router.put('/lists')
+router.put('/lists/:listId')
+
+// List items
+router.get('/lists/:listId/items/:itemId')
+router.post('/lists/:listId/items')
+router.put('/lists/:listId/items/:itemId')
 
 module.exports = router;
