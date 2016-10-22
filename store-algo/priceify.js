50ff8b59c34bb83a5ed03cf5af6fd59df945d3e5
Ryan Gaus <rsg1egoman@gmail.com>
2016-10-22 12:28:31 -0400

    code: consume a pantry!
    This function will, given a pantry, subtract anything from the recipe ingredients that is already on
    hand. Also, if the recipe uses that item, it will be subtracted from the pantry. Unfortunately, that
    means this function has side effects (on `pantryList`). Whatever though, it works.

--- store-algo/priceify.js
+++ store-algo/priceify.js
@@ -5,6 +5,41 @@ const util = require('util');
 let input = require('./grocerylist.json')
 let getItemPrice = require('./stores').getItemPrice;
 
+// a sample pantry
+let pantryList = [
+  {
+    _id: "cream-cheese",
+    quantity: "1 cup",
+  }
+];
+
+// This function will, given a pantry, subtract anything from the recipe ingredients that is already on
+// hand. Also, if the recipe uses that item, it will be subtracted from the pantry. Unfortunately, that
+// means this function has side effects (on `pantryList`). Whatever though, it works.
+function removePantryItemsFromList(list, pantryList) {
+  return list.map(item => {
+    // get the amount of the given item that's currently in the pantry
+    let pantryItem = pantryList.find(i => i._id === item._id);
+
+    if (pantryItem) {
+      // calculate the new quantity
+      let quantity = Unitz.subtract(item.quantity, pantryItem.quantity, true);
+      if (!quantity.startsWith('-')) {
+        // We need to buy some of the item since we don't have it on hand
+        return Object.assign({}, item, {quantity});
+      } else {
+        // we have too much! So we don't have to buy any.
+        // remove the amount from the pantry that this recipe takes up
+        pantryItem.quantity = quantity.slice(1);
+        return false;
+      }
+    } else {
+      return item; // that item isn't in the pantry
+    }
+  })
+  .filter(i => i !== false); // explicitly filter out the false that is specified above
+}
+
 // add an item to the items array while ensuring that duplicates are combined
 // When it combines dupes, it adds the quantities together.
 function addToItemsCombineDupes(initialItems, itemsToAdd) {
@@ -49,6 +84,8 @@ function parseList(list) {
 
 // Do the list assembly for realsies!
 let itemList = parseList(input);
+console.log('\n\n', util.inspect(removePantryItemsFromList(itemList, pantryList), false, Infinity, true));
+console.log('\n\n', util.inspect(pantryList, false, Infinity, true));
 
 let itemPricedList = itemList.map(item => {
   return {
@@ -56,4 +93,4 @@ let itemPricedList = itemList.map(item => {
     price: getItemPrice(item._id, item.quantity, item.store),
   };
 });
-console.log('\n\n', util.inspect(itemPricedList, false, Infinity, true));
+// console.log('\n\n', util.inspect(itemPricedList, false, Infinity, true));
